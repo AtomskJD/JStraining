e033e4a5504a3d46f07d88e45984dff872e94e46
AtomskJD <AtomskJD@gmail.com>
2015-07-02 17:51:47 +0500

    so slow

--- 17 http theory.js
+++ 17 http theory.js
@@ -7,3 +7,25 @@ req.send(null);
 console.log(req.status, req.statusText);
 console.log( req.getResponseHeader("content-type") );
 // console.log(req.responseText);
+
+// Асинхронн~~~
+
+var reqA = new XMLHttpRequest();
+reqA.open( "GET", "/", true);
+reqA.addEventListener( "load", function() {console.log("DONE", reqA.status);} );
+
+reqA.send(null);
+
+// Access-Control-Allow-Origin: *
+
+function backgroundReadFile( url, callback ) {
+  var req = new XMLHttpRequest();
+  req.open( "GET", url, true );
+  req.addEventListener( "load", function() {
+    if ( req.status < 400 ) {
+      callback( req.responseText );
+    }
+  });
+  
+  req.send( null );
+}
