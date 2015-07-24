5b03f4b64006bae0ccc590dcb2843212980d4f1d
Atomsk <atomskjd@gmail.com>
2015-07-24 20:47:52 +0500

    continue

---
+++ 20 (node) node promises.js
@@ -0,0 +1,10 @@
+var // libs
+  Promise = require("promise"),
+  fs = require("fs");
+
+var readFile = Promise.denodeify( fs.readFile );
+readFile("file.txt", "utf8").then(function( content ) {
+  console.log( "The file contained: " + content );
+}, function ( error ) {
+  console.log("Failed to read file: " + error );
+});
